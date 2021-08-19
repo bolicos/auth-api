37bb7f04bdcacb29799ba32a4863af31fd706525
analuciabolico <analuciabolico.dev@gmail.com>
2021-08-19 10:47:37 -0300

    change alghoritm of the Generation Token

--- src/main/java/com/github/analuciabolico/authapi/security/JwtTokenUtil.java
+++ src/main/java/com/github/analuciabolico/authapi/security/JwtTokenUtil.java
@@ -68,7 +68,7 @@ public class JwtTokenUtil implements Serializable {
                     .setSubject(subject)
                     .setIssuedAt(new Date(System.currentTimeMillis()))
                     .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-                    .signWith(SignatureAlgorithm.ES512, secret).compact();
+                    .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
 
     // valida o token
